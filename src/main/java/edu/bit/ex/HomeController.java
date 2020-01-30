1e26e10bee2c055ccadc41d179b48cf5de9f750d
lyeon615 <lyeon615@gmail.com>
2020-01-30 11:10:30 +0900

    add home3 commit and push

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -48,4 +48,18 @@ public class HomeController {
 		
 		return "home";
 	}
+	
+	@RequestMapping(value = "/home3", method = RequestMethod.GET)
+	public String home3(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
 }
